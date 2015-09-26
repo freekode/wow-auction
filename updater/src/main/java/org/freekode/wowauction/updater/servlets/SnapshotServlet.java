f049cc3947dae8e7d0adb7378c4f25aecf6ae9d0
Evgeny Frolov <efrolov.app@gmail.com>
2015-09-26 19:56:35 +0200

    try to make updater

--- updater/src/main/java/org/freekode/wowauction/updater/servlets/SnapshotServlet.java
+++ updater/src/main/java/org/freekode/wowauction/updater/servlets/SnapshotServlet.java
@@ -20,7 +20,7 @@ public class SnapshotServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("accounts", snapshotBean.findAll());
+        req.setAttribute("snapshots", snapshotBean.findAll());
 
         req.getRequestDispatcher("/snapshot.jsp").forward(req, resp);
     }
---
+++ updater/src/main/java/org/freekode/wowauction/updater/servlets/Test.java
@@ -0,0 +1,10 @@
+package org.freekode.wowauction.updater.servlets;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Test {
+    public void test() {
+        System.out.println("1!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+}
