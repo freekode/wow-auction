21b401d7d8ec59bccb677b4d8ad545f7caac62a0
Evgeny Frolov <efrolov.app@gmail.com>
2015-10-31 22:22:02 +0100

    fixed catalog sql

--- src/main/resources/db/migration/V1428437290__items_info.sql
+++ src/main/resources/db/migration/V1428437290__items_info.sql
@@ -10,8 +10,9 @@ CREATE TABLE items_info (
   itemSubclassId  INTEGER,
   inventorySlotId INTEGER,
   CONSTRAINT items_info_id_pk FOREIGN KEY (itemId) REFERENCES items (id) ON UPDATE RESTRICT ON DELETE CASCADE,
-  CONSTRAINT items_info_quality_fk FOREIGN KEY (itemId) REFERENCES catalogs (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
-  CONSTRAINT items_info_class_fk FOREIGN KEY (itemId) REFERENCES catalogs (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
-  CONSTRAINT items_info_subclass_fk FOREIGN KEY (itemId) REFERENCES catalogs (id) ON UPDATE RESTRICT ON DELETE RESTRICT
+  CONSTRAINT items_info_quality_fk FOREIGN KEY (qualityId) REFERENCES catalogs (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+  CONSTRAINT items_info_class_fk FOREIGN KEY (itemClassId) REFERENCES catalogs (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+  CONSTRAINT items_info_subclass_fk FOREIGN KEY (itemSubclassId) REFERENCES catalogs (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+  CONSTRAINT items_info_inventory_slot_fk FOREIGN KEY (inventorySlotId) REFERENCES catalogs (id) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
 
